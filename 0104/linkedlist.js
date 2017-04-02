c3751e5459b483f96f75f1b9f5f7adcab2282d37
Lili <insa.lilitong@gmail.com>
2017-04-03 00:04:58 +0200

    linkedlist ecmascript 6

--- 0104/linkedlist.js
+++ 0104/linkedlist.js
@@ -6,6 +6,7 @@ function Linkedlist(){
 	};
 	let length = 0;
 	let head = null;
+	//add a node to the end of the list
 	this.append = function(element){
 		let node = new Node(element),
 			current;
@@ -19,7 +20,8 @@ function Linkedlist(){
 			current.next = node;
 		}
 		length++;
-	}
+	};
+	//insert a node at a specific position
 	this.insert = function(position, elements){
 		if(position >= 0 && position <= length){
 			let node = new Node(element),
@@ -43,10 +45,65 @@ function Linkedlist(){
 		}else{
 			return false;
 		}
-	}
-	this.removeAt = function(){
-		
-	}
+	};
+	//remove a node at position
+	this.removeAt = function(position){
+		if(position >= 0 && position <= length){
+			let current = head,
+				previous,
+				index = 0;
+			if(position == 0){
+				head = current.next;
+			}else{
+				while(index++ < position){
+					previous = current;
+					current = current.next;
+				}
+				//connect the previous one and the next one
+				previous.next = current.next;
+			}
+			length--;
+			return current.element;
+		}else{
+			return null;
+		}
+	};
+	//get the index of an elements
+	this.indexOf = function(element){
+		les index = 0,
+			current = head;
+		while(current){
+			if(current.element === element) return index;
+			current = current.next;
+		}
+		return -1;
+	};
+	//remove a node
+	this.remove = function(element){
+		let index = this.indexOf(element);
+		return this.removeAt(index);
+	};
+	this.isEmpty = function(){
+		return length === 0;
+	};
+	this.size = function(){
+		return length;
+	};
+	this.getHead = function(){
+		return head;
+	};
+	this.toString = function(){
+		let current = head,
+			string = '';
+		while(current){
+			string += current.element + (current.next ? ', ':'');
+			current = current.next;
+		}
+		return string;
+	};
+	this.print = function(){
+		console.log(this.toString);
+	};
 }
 
 
---
+++ 0104/linkedlist2.js
@@ -0,0 +1,7 @@
+let LinkedList2 = (function(){
+	class Node {
+		constructor(element){
+			
+		}
+	}
+})
